1f14900fac25c1e2e1c40fbce8499916dae5ad62
asafsavich <asafsavich@gmail.com>
2016-02-10 00:26:27 +0200

    Fixed External Jar Problem

--- LabEx02/src/Program.java
+++ LabEx02/src/Program.java
@@ -1,18 +1,3 @@
-import java.net.UnknownHostException;
-import java.text.DateFormat;
-import java.util.Date;
-
-import org.omg.CORBA.DATA_CONVERSION;
-import org.xbill.DNS.ARecord;
-import org.xbill.DNS.Lookup;
-import org.xbill.DNS.Record;
-import org.xbill.DNS.Resolver;
-import org.xbill.DNS.SimpleResolver;
-import org.xbill.DNS.TextParseException;
-import org.xbill.DNS.Type;
-
-
-
 public class Program {
 
 	public static void main(String[] args) {
